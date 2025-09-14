48e93d1082a1c5b2c7a3142aef9803d8ffd6d7ee
Katherin <katherinlizeth.t@gmail.com>
2025-09-14 15:20:23 -0500

    Primer commit - Backend inicial con Django y JWT

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Company, Project, Story, Ticket
 from .serializers import CompanySerializer, ProjectSerializer, StorySerializer, TicketSerializer
 from rest_framework import viewsets
+from rest_framework import permissions
 
 # Create your views here.
 
@@ -21,5 +22,19 @@ class StoryViewSet(viewsets.ModelViewSet):
 
 
 class TicketViewSet(viewsets.ModelViewSet):
-    queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Permitir acceso sin autenticación
+
+
+    def get_queryset(self):
+        queryset = Ticket.objects.all().order_by('-created_at')
+        project = self.request.query_params.get('project', None)
+        story = self.request.query_params.get('story', None)
+
+        if project:
+            queryset = queryset.filter(story__project__id=project)
+
+        if story:
+            queryset = queryset.filter(story__id=story)
+
+        return queryset
